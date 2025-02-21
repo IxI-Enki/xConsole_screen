75c72fc942da90e51eb225add18f40933bff3130
Enki-VisualStudio <cell.enohp.apps@gmail.com>
2025-02-21 06:08:50 +0100

    initial commit

---
+++ Solution.Screen/Screen/Logic/GLOBAL_USINGS.cs
@@ -0,0 +1,7 @@
+﻿namespace Logic
+{
+        public class GLOBAL_USINGS
+        {
+
+        }
+}
---
+++ Solution.Screen/Screen/ScreenUnitTest/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace ScreenUnitTest
+{
+        [TestClass]
+        public sealed class Test1
+        {
+                [TestMethod]
+                public void TestMethod1( )
+                {
+                }
+        }
+}
